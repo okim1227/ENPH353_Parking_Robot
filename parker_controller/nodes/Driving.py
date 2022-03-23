8449b77b717b5687cae2204fa2b855590d7f0206
okim1227 <okim1227@gmail.com>
2022-03-22 17:38:23 -0700

    Clock test working in Driver node

--- parker_controller/nodes/Driving.py
+++ parker_controller/nodes/Driving.py
@@ -2,9 +2,11 @@
 from __future__ import print_function
 
 import roslib
+#roslib.load_manifest('parker_controller')
 import sys
 import rospy
 import cv2
+import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -14,29 +16,31 @@ class driver:
 
   def __init__(self):
 
-    self.bridge = CvBridge()
+    #self.bridge = CvBridge()
 
     #self.pedestrian_sub = rospy.Subscriber("/rrbot/camera1/image_raw",Image,self.callback)
     #self.truck_sub = rospy.Subscriber("/rrbot/camera1/image_raw",Image,self.callback)
     
     #self.camera_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.camera_callback)
-    self.clock_sub = rospy.Subscriber("/clock",String,self.clock_callback)
 
     #self.driver_pub = rospy.Publisher("/driver",Twist)
     #self.cmdVel_pub = rospy.Publisher("/R1/cmd_vel",Twist)
-    self.license_pub = rospy.Publisher("/license_plate", String)
+    self.license_pub = rospy.Publisher("/license_plate", String, queue_size=1)
 
-    self.license_pub.publish(str('TeamRed,multi12, -1, XR58'))
+    #self.clock_sub = rospy.Subscriber("/clock",String,self.clock_callback)
+
+    time.sleep(1)
+
+    self.license_pub.publish("NO_NAME,multi12,0,XR58")
 
 
   #def clock_callback(self,data):
-    #print data
+    #print(data)
 
+rospy.init_node("driver", anonymous=True)
 
 def main(args):
   ic = driver()
-  rospy.init_node("driver", anonymous=True)
-
 
 if __name__ == '__main__':
   main(sys.argv)
