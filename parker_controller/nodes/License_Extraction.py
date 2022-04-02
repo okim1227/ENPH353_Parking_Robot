51e900c3ade77408793b80a33923f6a2500772d9
SabrinaAshik <sabrinapriyaashik04@gmail.com>
2022-04-02 15:45:42 -0700

    binary thresholding

--- parker_controller/nodes/License_Extraction.py
+++ parker_controller/nodes/License_Extraction.py
@@ -21,7 +21,10 @@ class license_extraction:
 
 	def camera_callback(self,data):
 		cv_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
-		plt.imshow(cv_img, cmap='gray', vmin=0, vmax=255)
+		gray = cv2.cvtColor(cv_img, cv2.COLOR_RGB2GRAY)
+		_, binary = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY_INV)
+		crop_img = binary[450:600, 0:350]
+		plt.imshow(crop_img, cmap='gray', vmin=0, vmax=255)
 		plt.show()
 
 def main(args):
