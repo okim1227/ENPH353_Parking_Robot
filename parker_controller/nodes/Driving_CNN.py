3d1ac63daf2584bbd39eca2cd9954c7cfaca8f77
SabrinaAshik <sabrinapriyaashik04@gmail.com>
2022-04-07 13:39:26 -0700

    commit

--- parker_controller/nodes/Driving_CNN.py
+++ parker_controller/nodes/Driving_CNN.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+import sys
+import rospy
+import cv2
+import time
+import numpy as np
+import matplotlib.pyplot as plt 
+import os
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
 import tensorflow as tf
 from tensorflow.keras import models
 from tensorflow.python.keras.backend import set_session
@@ -9,17 +23,35 @@ import numpy as np
 sess1 = tf.Session()    
 graph1 = tf.get_default_graph()
 set_session(sess1)
+driving_NN = models.load_model("/home/fizzer/ros_ws/src/parker_controller/nodes/driving_CNN_model.h5")
 
-driving_NN = models.load_model("driving_CNN_model.h5")
 
-def predict(image):
-	global sess1
-	global graph1
-	with graph1.as_default():
-		set_session(sess1)
-		NN_prediction = driving_NN.predict(image)[0]
-	return NN_prediction
+class driving_cnn:
 
-blank_image = np.array([np.zeros((125, 105, 3))])
+	def __init__(self):
+		self.bridge = CvBridge()
+		self.cmdVel_pub = rospy.Publisher("/R1/cmd_vel", Twist)
+		self.camera_sub = rospy.Subscriber("/R1/camera1/image_raw",Image,self.callback)
+		time.sleep(1)
 
-print(predict(blank_image))
+	def callback(self, image):
+		cv_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		message = self.predict(cv_img)
+		self.cmdVel_pub.publish(message)
+
+	
+	def predict(slef, image):
+		global sess1
+		global graph1
+		with graph1.as_default():
+			set_session(sess1)
+			NN_prediction = driving_NN.predict(image)[0]
+		return NN_prediction
+
+def main(args):
+	rospy.init_node("driving_cnn", anonymous=True)
+	ic = driving_cnn()
+	rospy.spin()
+
+if __name__ == '__main__':
+  main(sys.argv)
