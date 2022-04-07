588130b05d66984ac0025c3b4a3195c997640b4f
okim1227 <okim1227@gmail.com>
2022-04-06 20:55:21 -0700

    updated Driving CNN to save twist commands as image file names

--- parker_controller/nodes/Driving_CNN_Test.py
+++ parker_controller/nodes/Driving_CNN_Test.py
@@ -24,31 +24,35 @@ os.chdir(directory)
 class Driving_CNN_Test:
 
   def __init__(self):
-    self.bridge = CvBridge()
     # self.car_maual_driver_sub = rospy.Subscriber("/gazebo/model_states", ModelStates, self.CNN_callback)
     self.car_maual_driver_sub = rospy.Subscriber("/R1/cmd_vel", Twist, self.car_manual_driver_callback)
     self.camera_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.camera_callback, queue_size=100)
+    self.bridge = CvBridge()
     self.speed  = []
     self.twist_data = []
     self.capture_index = 0
     time.sleep(1)
 
-
   def car_manual_driver_callback(self, msg):
-    self.speed = msg
+    self.speed_linear = msg
 
   def camera_callback(self, msg):
     #print("Received an image!")
 
     # Append the current twist command when camera callback is called
     self.twist_data.append(self.speed)
-    print(self.twist_data[self.capture_index])
+    print(self.twist_data)
+
+    # output_twist_file = open("Twist_Commands.txt", "w")
+    # output_twist_file.write(self.twist_data)
+    # output_twist_file.close()
 
     # Save the captured image from camera subscriber to the Driving Views folder
+    # Name of capture is Capture_{linear_x command}_{angular_z command}
     cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-    cv2.imwrite(os.path.join(directory + "/Driving_Views/", "Capture_{}.png".format(self.capture_index)), cv_image)
+    cv2.imwrite(os.path.join(directory + "/Driving_Data/", "Capture_{}_{}.png".format(self.speed.linear.x, self.speed.angular.z)), cv_image)
+    
     self.capture_index = self.capture_index + 1
-
     print(self.capture_index)
 
 rospy.init_node("Driving_CNN_Test", anonymous=True)
